b011b48192e383acfbb6b831bd8c24e5905ed3b6
RonathTharana <147051007+RonathTharana@users.noreply.github.com>
2024-02-01 16:21:28 +0530

    Initial commit

---
+++ lib/widgets/custom_scaffold.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomScaffold extends StatelessWidget {
+  const CustomScaffold({super.key, this.child});
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.green),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      extendBodyBehindAppBar: true,
+      body: Stack(
+          children: [
+            Image.asset(
+              'assets/images/Hlogin3.jpg',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            SafeArea(
+              child: child!,
+            ),
+          ]
+      ),
+    );
+  }
+}
+
