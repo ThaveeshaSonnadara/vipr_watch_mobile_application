89ad3fd182b531184d6b592dc588ef6b067fb951
dinuka-himasha23 <130582863+dinuka-himasha23@users.noreply.github.com>
2024-03-07 01:27:24 +0530

    creating a search page

--- lib/repositories/authentication_repository.dart
+++ lib/repositories/authentication_repository.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:vipr_watch_mobile_application/screens/search_page/SearchPage.dart';
 
 import '../screens/login/login.dart';
 import '../screens/onboarding/onboarding.dart';
@@ -19,8 +20,11 @@ class AuthenticationRepository extends GetxController {
   screenRedirect() async {
     deviceStorage.erase();
     deviceStorage.writeIfNull('isFirstTime', true);
+    // deviceStorage.read('isFirstTime') != true
+    //     ? Get.offAll(() => const LoginScreen())
+    //     : Get.offAll(const OnBoardingScreen());
     deviceStorage.read('isFirstTime') != true
-        ? Get.offAll(() => const LoginScreen())
-        : Get.offAll(const OnBoardingScreen());
+        ? Get.offAll(() => SearchPage())
+        : Get.offAll(SearchPage());
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import connectivity_plus
 import firebase_core
 import path_provider_foundation
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
