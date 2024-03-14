76526b26792d301b936aaa6472c5a32ac7056e9e
DNethmini <153075755+DNethmini@users.noreply.github.com>
2024-03-14 13:53:38 +0530

    add scrollview for home page and about us

--- lib/screens/HomeScreen/AboutUs.dart
+++ lib/screens/HomeScreen/AboutUs.dart
@@ -1,118 +1,84 @@
 import 'package:flutter/material.dart';
 
-class AboutUs extends StatelessWidget{
-  const AboutUs({super.key});
+class AboutUs extends StatelessWidget {
+  const AboutUs({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back,color: Colors.white,),
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: const Column(
-        children: [
-          SizedBox(height: 20,),
-          Center(
-            child: Text(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: screenHeight * 0.05),
+            Text(
               "Team Elites",
-                  style:TextStyle(color: Colors.green,fontSize: 35,fontWeight: FontWeight.bold),
+              style: TextStyle(color: Colors.green, fontSize: screenWidth * 0.1, fontWeight: FontWeight.bold),
             ),
-          ),
-          SizedBox(height: 50,),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Text(
-              "We are a group of second-year university students following the BEng(Hons)Software Engineering degree at the Informatics Insititute of Technology in collaboration with University of Westminster,UK.",
-                style: TextStyle(color: Colors.white, fontSize: 16),
-            textAlign: TextAlign.justify,
+            SizedBox(height: screenHeight * 0.05),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+              child: Text(
+                "We are a group of second-year university students following the BEng(Hons)Software Engineering degree at the Informatics Institute of Technology in collaboration with University of Westminster, UK.",
+                style: TextStyle(color: Colors.white, fontSize: screenWidth * 0.04),
+                textAlign: TextAlign.justify,
+              ),
             ),
-          ),
-          SizedBox(height: 70,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 10),
-              ),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pics/thaveesha.jpg'),
-                radius: 50,
-              ),
-              SizedBox(width: 20),
-              Text(
-                'Thaveesha\n Thilan',
-                style: TextStyle(color: Colors.white),
-              ),
-              SizedBox(width: 20), // Increased spacing between items
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pics/ronath.jpg'),
-                radius: 50,
-              ),
-              SizedBox(width: 10),
-              Text(
-                ' Ronath\n Tharana',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
-          SizedBox(height: 70), // Spacing between rows
-          // Second row with two images
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 20),
-              ),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pics/isanka.jpg'),
-                radius: 50,
-              ),
-              SizedBox(width: 10),
-              Text(
-                ' Isanka\n Vimukthi',
-                   style: TextStyle(color: Colors.white),
-              ),
-              SizedBox(width: 20),
-              Padding(
-                padding: EdgeInsets.only(right: 25),
-              ),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pics/dinuka.jpg'),
-                radius: 50,
-              ),
-              SizedBox(width: 10),
-              Text(
-                ' Dinuka\n Himasha',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
-          SizedBox(height: 70),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(right: 30),
-              ),
-              CircleAvatar(
-                backgroundImage: AssetImage('assets/images/pics/durangi.jpg'),
-                radius: 50,
-              ),
-              SizedBox(width: 10),
-              Text(
-                '  Durangi\n  Nethmini',
-                style: TextStyle(color: Colors.white),
-              ),
-            ],
-          ),
-        ],
+            SizedBox(height: screenHeight * 0.1),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildAvatarWithName('assets/images/pics/thaveesha.jpg', 'Thaveesha\nThilan', screenWidth),
+                SizedBox(width: screenWidth * 0.22),
+                _buildAvatarWithName('assets/images/pics/ronath.jpg', 'Ronath\nTharana', screenWidth),
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.05),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildAvatarWithName('assets/images/pics/isanka.jpg', 'Isanka\nVimukthi', screenWidth),
+                SizedBox(width: screenWidth * 0.22),
+                _buildAvatarWithName('assets/images/pics/dinuka.jpg', 'Dinuka\nHimasha', screenWidth),
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.05),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                _buildAvatarWithName('assets/images/pics/durangi.jpg', '   Durangi\n   Nethmini', screenWidth),
+              ],
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildAvatarWithName(String imagePath, String name, double screenWidth) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundImage: AssetImage(imagePath),
+          radius: screenWidth * 0.1,
+        ),
+        SizedBox(height: 10),
+        Text(
+          name,
+          style: TextStyle(color: Colors.white),
+        ),
+      ],
     );
   }
 }
