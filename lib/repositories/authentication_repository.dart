5247e0a165801661736e6b902617ef3c5f3ce7a2
Thaveesha Sonnadara <sonnadarathaveesha@gmail.com>
2024-03-17 23:59:02 +0530

    corrected some import statements

--- lib/repositories/authentication_repository.dart
+++ lib/repositories/authentication_repository.dart
@@ -2,7 +2,7 @@ import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:vipr_watch_mobile_application/utills/login_or_register.dart';
-import '../screens/onboarding/onboarding.dart';
+import 'package:vipr_watch_mobile_application/screens/onboarding/onboarding.dart';
 
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
@@ -19,7 +19,7 @@ class AuthenticationRepository extends GetxController {
     deviceStorage.erase();
     deviceStorage.writeIfNull('isFirstTime', true);
     deviceStorage.read('isFirstTime') != true
-        ? Get.offAll(() =>  const LoginOrRegister ())
-        : Get.offAll(  const OnBoardingScreen ());
+        ? Get.offAll(() => const LoginOrRegister())
+        : Get.offAll(const OnBoardingScreen());
   }
 }
