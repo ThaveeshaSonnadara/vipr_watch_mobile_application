cd69b167fe208e2ba491e8739a0053b8400403e7
Thaveesha Sonnadara <sonnadarathaveesha@gmail.com>
2024-03-23 01:02:07 +0530

    change the POST request url

--- lib/screens/image_input/identify_snake.dart
+++ lib/screens/image_input/identify_snake.dart
@@ -20,7 +20,7 @@ class IdentifySnake {
     // Pass the image path to the api endpoint Url
     // var request = http.MultipartRequest('POST', Uri.parse('http://13.53.168.135:8080/predict'));
     var request = http.MultipartRequest(
-        'POST', Uri.parse('http://54.82.43.114:8080/predict'));
+        'POST', Uri.parse('http://54.210.211.13:8080/predict'));
 
     var picture = await http.MultipartFile.fromPath('file', imageFile);
 
